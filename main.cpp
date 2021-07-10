e29dcb5c75639e1c54c39992e81286bb100d938a
TWVerstraaten <t.w.verstraaten@rug.nl>
2021-07-10 14:31:26 +0200

    Namespacing action

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "app/CentralWidget.h"
-#include "prs/AstManager.h"
+#include "ast/AstManager.h"
 
 #include <QApplication>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    Ast a1{"f(y)=a*(x,y)"};
+    ast::Ast a1{"f(y)=a*(x,y)"};
     std::cout << a1.toStringFlat() << "\t csts:\t";
     for (const auto& el : a1.constantDependencies()) {
         std::cout << el << "\t";
@@ -20,8 +20,8 @@ int main(int argc, char* argv[]) {
     //
     //    std::cout << m.toString() << '\n';
 
-    QApplication       a(argc, argv);
-    app::CentralWidget w;
-    w.show();
-    return a.exec();
+    //    QApplication       a(argc, argv);
+    //    app::CentralWidget w;
+    //    w.show();
+    //    return a.exec();
 }
